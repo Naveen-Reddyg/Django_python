e71a75782b572506234826d4034581edf138bb4c
Naveen <naveenreddy3793@gmail.com>
2020-03-11 12:34:20 -0700

    Dynamic data with DB

--- travello/views.py
+++ travello/views.py
@@ -4,24 +4,6 @@ from .models import Destination
 # Create your views here.
 def index(request):
     
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = "its the financial capital of India"
-    dest1.price = 700
-    dest1.offer = False
-    dest1.img = 'destination_1.jpg'
-    dest2 = Destination()
-    dest2.name = 'Hyderabad'
-    dest2.desc = 'Place for Biryani'
-    dest2.price = 500
-    dest2.offer = True
-    dest2.img = 'destination_2.jpg'
-    dest3 = Destination()
-    dest3.name = 'Bangalore'
-    dest3.desc = 'India Silicon Valley'
-    dest3.price = 800
-    dest3.offer = False
-    dest3.img = 'destination_3.jpg'
-    dests = [dest1,dest2,dest3]
-
+    dests = Destination.objects.all()
+    
     return render(request,'index.html',{'dests' : dests})
